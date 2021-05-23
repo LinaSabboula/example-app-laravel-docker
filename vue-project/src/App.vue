63a144030e37d89b6050f72d9b0189763f16402e
Lina <linamuhab@gmail.com>
2021-05-23 10:46:00 +0200

    Registering AddGovernment component in App

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,9 +2,11 @@
      <add-government></add-government>
 </template>
 
-<script setup>
-import AddGovernment from './components/AddGovernment.vue'
-
+<script>
+import AddGovernment from "./components/AddGovernment.vue";
+export default {
+    components: {'add-government': AddGovernment}
+}
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
