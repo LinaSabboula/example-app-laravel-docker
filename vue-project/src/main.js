81ea98008d0eb3ed13561ffd2fe3e5fec3382704
Lina <linamuhab@gmail.com>
2021-05-23 18:05:23 +0200

    Globally register all components

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import AddGovernment from "./components/AddGovernment.vue";
+import ButtonComponent from "./components/ButtonComponent.vue";
+import LabelComponent from "./components/LabelComponent.vue";
+import TextInputComponent from "./components/TextInputComponent.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('add-government', AddGovernment);
+app.component('label-component', LabelComponent);
+app.component('button-component', ButtonComponent);
+app.component('text-input-component', TextInputComponent);
+app.mount('#app');
+
