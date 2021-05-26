a322a3de158adaf6e8ee43d685de234d147375ae
Lina <linamuhab@gmail.com>
2021-05-26 10:54:13 +0200

    Added a dropdownList component

---
+++ vue-project/src/components/dropdownListComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <label-component :label-text="labelText"></label-component>
+    <select name="labelText"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)">
+        <option value="" disabled selected>
+            Select your option
+        </option>
+        <option
+            v-for="item in items"
+            :key="item.id"
+            :value=item.id>
+            {{ item.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+
+export default {
+    name: "dropdownListComponent",
+    props: {
+        'placeholderValue': {
+            String,
+        },
+        'modelValue':{
+            type: String
+        },
+        'items': {
+            type: Array,
+            required: true,
+            default: [],
+        },
+        'labelText': {
+            type: String,
+        },
+        'inputName': {
+            type: String,
+        }
+    },
+}
+</script>
+
