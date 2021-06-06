cb3cc44f02dcfd7d7cbdd4183c58145a926ce27e
Lina <linamuhab@gmail.com>
2021-06-06 17:19:48 +0200

    Dynamically iterate over object

--- nova-components/GovernmentManager/resources/js/components/TableComponent.vue
+++ nova-components/GovernmentManager/resources/js/components/TableComponent.vue
@@ -9,12 +9,8 @@
         </tr>
         </thead>
         <tbody>
-        <tr v-for="(item, index) in this.body" :key="index">
-            <td>{{ item.id }}</td>
-            <td>{{ item.name }}</td>
-            <td>{{ item.active }}</td>
-            <td>{{ item.created }}</td>
-            <td>{{ item.updated }}</td>
+        <tr v-for="(item, index) of this.body" :key="index">
+            <td v-for="(property, index) in item" :key="index">{{ property }}</td>
         </tr>
         </tbody>
     </table>
